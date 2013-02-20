89a17f4809fbbcf96d3b8e852b28c0f320deb254
facascante <cfajr@rocketmail.com>
2013-02-20 08:03:46 +0800

    add api registration api

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 express()
 .use(config.global)
-.use(express.vhost(config.PROFILEWEB_URL, require('./service')))
+.use(express.vhost('localhost', require('./service')))
 .listen(config.PORT,function(){
 	 console.log("----------------Connect Auth READY----------------");
 });
